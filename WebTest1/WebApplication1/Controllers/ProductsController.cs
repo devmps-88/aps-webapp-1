af0ca3ec0a201d89001bfeb1660465fba6222cf4
devmps-88 <79127673+devmps-88@users.noreply.github.com>
2021-05-18 18:05:35 +0700

    (M) Add project.

---
+++ WebTest1/WebApplication1/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            this.ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+    }
+}
